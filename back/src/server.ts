6ed13410b960f415089a7a4eb07c69ba7af37ccf
GabrielLuisBS <gabriel.08silva24@gmail.com>
2026-08-18 22:24:56 -0300

    scaffold do back (Fastify 5 + TS iniciado)

---
+++ back/src/server.ts
@@ -0,0 +1,9 @@
+import Fastify from "fastify";
+
+const app = Fastify({ logger: true });
+const port = Number(process.env.PORT ?? 3333);
+
+app.listen({ port, host: "0.0.0.0" }).catch((err) => {
+  app.log.error(err);
+  process.exit(1);
+});
